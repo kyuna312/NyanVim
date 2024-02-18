c8c5dd85cafd44f7013689570f9eef369ccb4ece
Marin <khatanzorigb@gmail.com>
2024-02-18 23:40:07 +0800

    nyan vim chad config operation changes

--- init.lua
+++ init.lua
@@ -1,11 +1,21 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-if vim.loader then
-  vim.loader.enable()
+require "core"
+
+local custom_init_path = vim.api.nvim_get_runtime_file("lua/custom/init.lua", false)[1]
+
+if custom_init_path then
+  dofile(custom_init_path)
 end
 
-_G.dd = function(...)
-  require("util.debug").dump(...)
-end
-vim.print = _G.dd
+require("core.utils").load_mappings()
 
-require("config.lazy")
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
+-- bootstrap lazy.nvim!
+if not vim.loop.fs_stat(lazypath) then
+  require("core.bootstrap").gen_chadrc_template()
+  require("core.bootstrap").lazy(lazypath)
+end
+
+dofile(vim.g.base46_cache .. "defaults")
+vim.opt.rtp:prepend(lazypath)
+require "plugins"
